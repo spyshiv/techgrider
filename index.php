fe6f4bf9f9ed017c52bea400eb45296e455555cf
spyshiv <spyshiv@gmail.com>
2014-10-13 03:00:28 -0700

    Head and preference links for css and javascript are added

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<title>Tech-Grider|Home</title>
+<!--link for css And Js Properties-->
+<link type="text/css" rel="stylesheet" href="includes/overall/decoration/css/cssindex.css"/>
+<script type="text/javascript" src="includes/overall/decoration/js/jsindex.js"></script>
+<!---->
+<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="includes/overall/engine1/style.css" media="screen" />
+	<script type="text/javascript" src="includes/overall/engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+</head>
+
+</html>
