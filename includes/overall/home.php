da37b4a8cccaabc0892e4a3e0e92931155db5423
spyshiv <spyshiv@gmail.com>
2014-10-15 02:35:19 -0700

    A jquery button for back to top is added

--- includes/overall/home.php
+++ includes/overall/home.php
@@ -15,7 +15,30 @@
      <div id="whole" align="center">
       <h1 id="home">Home</h1>
          <div class="row">
+
        </div>
+
+       <a href="#" class="back-to-top">Back to Top</a>
+        <script>
+            
+			jQuery(document).ready(function() {
+				var offset = 220;
+				var duration = 500;
+				jQuery(window).scroll(function() {
+					if (jQuery(this).scrollTop() > offset) {
+						jQuery('.back-to-top').fadeIn(duration);
+					} else {
+						jQuery('.back-to-top').fadeOut(duration);
+					}
+				});
+				
+				jQuery('.back-to-top').click(function(event) {
+					event.preventDefault();
+					jQuery('html, body').animate({scrollTop: 0}, duration);
+					return false;
+				})
+			});		
+</script>
      </div>
 </body>
 </html>
