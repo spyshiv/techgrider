3f02fbc8710f4cfb4c790bd0273a43b3e2015ce2
spyshiv <spyshiv@gmail.com>
2014-10-15 02:20:50 -0700

    Error removed in commitment

---
+++ includes/overall/home.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<title>Tech-Grider|Home</title>
+<!--link for css And Js Properties-->
+<link type="text/css" rel="stylesheet" href="decoration/css/cssindex.css"/>
+<link type="text/css" rel="stylesheet" href="decoration/css/home.css">
+<script type="text/javascript" src="decoration/js/jsindex.js"></script>
+<script src="decoration/js/jquery-2.1.1.min.js"></script>
+<!---->
+<!---->
+<!---->
+</head>
+<body>
+      
+
+</body>
+</html>
