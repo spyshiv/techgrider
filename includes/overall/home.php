ac2c1d7b7cc4485453ec6b7f7763436a3e788818
spyshiv <spyshiv@gmail.com>
2014-10-16 02:46:07 -0700

    First technical websites div is added and references are added

--- includes/overall/home.php
+++ includes/overall/home.php
@@ -15,6 +15,18 @@
      <div id="whole" align="center">
       <h1 id="home">Home</h1>
          <div class="row">
+         <div class="element">
+			      <div class="element_head">
+					<img src="Images/newsImg/verge.png" height="32px" width="32px" />
+			      </div>
+				  <a href="result.php?link=theverge">
+				    <div><strong><p>The Verge</p></strong></div>
+				  </a>	
+				  <a href="check.php?link=theverge"><div class="add_to_library" id="the_verge"><b><p title="Add To Library" align="center">+</p></b>
+				  </div></a>
+				  <p>The Verge was founded in 2011 in partnership with Vox Media, and covers the intersection of technology, science, art, and culture.</p>
+				  <a href="result.php?link=theverge"><center><img src="Images/newsImg/wallpaper/Verge.jpg" height="200px" width="300px" /></center></a>
+			  </div>
 
        </div>
 
