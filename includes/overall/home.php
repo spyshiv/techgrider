891569c3d7cafd9d91eb91f72602b3d21d5303b4
spyshiv <spyshiv@gmail.com>
2014-10-17 02:54:44 -0700

    Second row is finished

--- includes/overall/home.php
+++ includes/overall/home.php
@@ -69,33 +69,35 @@
 				  <p>The latest news, analysis and conversation in all things web, tech and social media from the http://t.co/rp2Ar....</p>
 				  <a href="result.php?link=readwrite"><center><img src="Images/newsImg/wallpaper/Readwrite.jpg" height="200px" width="300px" /></center></a>
 			  </div>
-       </div>
+ 
 
-       <div class="element">
-			     <div class="element_head">
-                  <img src="Images/newsImg/Techcrunch.png" height="32px" width="32px" />
-				  </div>
-				  <a href="result.php?link=techcrunch"><div>
-				  <strong><p>Tech Crunch</p></strong>				 
-				 </div></a>
-				  <a href="check.php?link=techcrunch"><div  class="add_to_library" id="tech_crunch"><b><p title="Add To Library" align="center">+</p></b>
-				  </div></a>
-				 <p>TechCrunch is a group-edited blog that profiles the companies, products and events defining and transforming t...</p>
-				 <a href="result.php?link=techcrunch"><center><img src="Images/newsImg/wallpaper/Techcrunch.jpg" height="200px" width="300px" /></center></a> 
-		      </div>
-		      
-		 <div class="element">
-			     <div class="element_head">
-                  <img src="Images/newsImg/Gizmodo.png" height="32px" width="32px" />
-				  </div>
-				  <a href="result.php?link=gizmodo"><div>
-				  <strong><p>Gizmodo</p></strong>				 
-				 </div></a>
-				 <a href="check.php?link=gizmodo"><div  class="add_to_library" id="gizmodo"><b><p title="Add To Library" align="center">+</p></b>
-				  </div></a>
-				 <p>Gizmodo is a design and technology blog. It is part of the Gawker Media network run by Nick Denton and runs on the Kinja platform</p>
-				 <a href="result.php?link=gizmodo"><center><img src="Images/newsImg/wallpaper/Gizmodo.jpg" height="200px" width="300px" /></center></a>
-		      </div>
+	       <div class="element">
+				     <div class="element_head">
+	                  <img src="Images/newsImg/Techcrunch.png" height="32px" width="32px" />
+					  </div>
+					  <a href="result.php?link=techcrunch"><div>
+					  <strong><p>Tech Crunch</p></strong>				 
+					 </div></a>
+					  <a href="check.php?link=techcrunch"><div  class="add_to_library" id="tech_crunch"><b><p title="Add To Library" align="center">+</p></b>
+					  </div></a>
+					 <p>TechCrunch is a group-edited blog that profiles the companies, products and events defining and transforming t...</p>
+					 <a href="result.php?link=techcrunch"><center><img src="Images/newsImg/wallpaper/Techcrunch.jpg" height="200px" width="300px" /></center></a> 
+			      </div>
+
+			 <div class="element">
+				     <div class="element_head">
+	                  <img src="Images/newsImg/Gizmodo.png" height="32px" width="32px" />
+					  </div>
+					  <a href="result.php?link=gizmodo"><div>
+					  <strong><p>Gizmodo</p></strong>				 
+					 </div></a>
+					 <a href="check.php?link=gizmodo"><div  class="add_to_library" id="gizmodo"><b><p title="Add To Library" align="center">+</p></b>
+					  </div></a>
+					 <p>Gizmodo is a design and technology blog. It is part of the Gawker Media network run by Nick Denton and runs on the Kinja platform</p>
+					 <a href="result.php?link=gizmodo"><center><img src="Images/newsImg/wallpaper/Gizmodo.jpg" height="200px" width="300px" /></center></a>
+			      </div>
+	     </div>
+         </br></br>
 
        <a href="#" class="back-to-top">Back to Top</a>
         <script>
