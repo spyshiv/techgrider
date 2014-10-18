9540af5609ba76b53a672a3d27edfa18bf64ed11
spyshiv <spyshiv@gmail.com>
2014-10-18 03:01:43 -0700

    Fifth row is started

--- includes/overall/home.php
+++ includes/overall/home.php
@@ -141,6 +141,10 @@
          </div>
           </br></br>
 
+          <div class="row">
+	            
+	       </div>
+
        <a href="#" class="back-to-top">Back to Top</a>
         <script>
             
