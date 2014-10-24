f8617ea8828d8baf9cc00464c9afb1d0ebfbe253
spyshiv <spyshiv@gmail.com>
2014-10-24 05:48:55 -0700

    welcome file is created and profile id is generated to fetch user name

---
+++ includes/database/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Your Home Page</title>
+
+</head>
+<body>
+<div id="profile">
+<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+<b id="logout"><a href="../database/logout.php">Log Out</a></b>
+</div>
+</body>
+</html>
