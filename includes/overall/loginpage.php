3488b7f6563bd917fd6361ccd54c4568c7afc2fd
spyshiv <spyshiv@gmail.com>
2014-10-26 06:07:04 -0700

    loginpage base is created

---
+++ includes/overall/loginpage.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="decoration/css/signstyle1.css" />
+<!--link for css And Js Properties-->
+<link type="text/css" rel="stylesheet" href="decoration/css/cssindex.css"/>
+<script type="text/javascript" src="decoration/js/jsindex.js"></script>
+<!---->
+</head>
+<body>
+	
+</body>
+</html>
