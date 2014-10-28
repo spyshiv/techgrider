2bc534c4b75f68669ffbc8c9bc31e2d13aa05540
spyshiv <spyshiv@gmail.com>
2014-10-28 06:28:03 -0700

    Database is connected

--- includes/database/session.php
+++ includes/database/session.php
@@ -5,5 +5,10 @@ $connection = mysql_connect("localhost", "root", "");
 $db = mysql_select_db("shiv", $connection);
 session_start();// Starting Session
 // Storing Session
+$user_check=$_SESSION['login_user'];
+// SQL Query To Fetch Complete Information Of User
+$ses_sql=mysql_query("select username from users where username='$user_check'", $connection);
+$row = mysql_fetch_assoc($ses_sql);
+$login_session =$row['username'];
 
   ?>
