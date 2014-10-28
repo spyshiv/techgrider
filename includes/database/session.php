e1f8cd36bfd4d715e6b7d723d54567493d894bc6
spyshiv <spyshiv@gmail.com>
2014-10-28 06:28:54 -0700

    Session Checking is added

--- includes/database/session.php
+++ includes/database/session.php
@@ -10,5 +10,10 @@ $user_check=$_SESSION['login_user'];
 $ses_sql=mysql_query("select username from users where username='$user_check'", $connection);
 $row = mysql_fetch_assoc($ses_sql);
 $login_session =$row['username'];
+if(!isset($login_session)){
+mysql_close($connection); // Closing Connection
+header('Location: ../overall/signup.php'); // Redirecting To Home Page
+exit();
+}
 
   ?>
