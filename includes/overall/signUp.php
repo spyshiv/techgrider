c2e97d7b46f7817dbe9ecb0aa15e0ac552048c96
spyshiv <spyshiv@gmail.com>
2014-10-31 06:57:13 -0700

    Basic static structure for signup is created

---
+++ includes/overall/signUp.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>Tech-Grider|Sign Up</title>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="decoration/css/signstyle1.css" />
+	<!--<link type="text/css" rel="stylesheet" href="style.css"/>-->
+	<link type="text/css" rel="stylesheet" href="decoration/css/cssindex.css"/>
+<script type="text/javascript" src="decoration/js/jsindex.js"></script>
+</head>
+
+<body>
+</body>
+</html>
