156558cd5f6167396a7c0907d843cad8d74f2971
spyshiv <spyshiv@gmail.com>
2014-11-03 00:22:32 -0800

    Frame for result page is added

---
+++ includes/overall/result.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html"  charset="utf-8" />
+<meta http-equiv="content-language" content="en" />
+<title>Tech-Grider|News</title>
+<link type="text/css" rel="stylesheet" href="decoration/css/cssindex.css"/>
+<script type="text/javascript" src="decoration/js/jsindex.js"></script>
+<link href="decoration/css/result.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	
+</body>
