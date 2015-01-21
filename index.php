194a5c3e3bc7ac5436e8890b8325a87738c19ae6
spyshiv <spyshiv@gmail.com>
2015-01-21 01:22:19 -0800

    Slider is added on the index page

--- index.php
+++ index.php
@@ -38,6 +38,34 @@
 	 <div>
 	  <center><a href="includes/overall/loginpage.php"><img src="includes/overall/images/start.png" width="100px" height="auto" alt="start_read"/></a></center>
 	  <br>
+	  <!-- Start WOWSlider.com BODY section id=wowslider-container1 -->
+	<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+<li><img src="includes/overall/data1/images/pic1.jpg"  id="wows1_0"/></li>
+<li><img src="includes/overall/data1/images/pic2.jpg"  id="wows1_1"/></li>
+<li><img src="includes/overall/data1/images/pic3.jpg"  id="wows1_2"/></li>
+<li><img src="includes/overall/data1/images/pic4.jpg"  id="wows1_3"/></li>
+<li><img src="includes/overall/data1/images/pic5.jpg"  id="wows1_4"/></li>
+<li><img src="includes/overall/data1/images/pic6.jpg"  id="wows1_5"/></li>
+<li><img src="includes/overall/data1/images/pic7.jpg"  id="wows1_6"/></li>
+<li><img src="includes/overall/data1/images/plant.jpg" id="wows1_7"/></li>
+<li><img src="includes/overall/data1/images/technology.jpg" id="wows1_8"/></li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/pic1.jpg" />1</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/pic2.jpg" />2</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/pic3.jpg" />3</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/pic4.jpg" />4</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/pic5.jpg" />5</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/pic6.jpg" />6</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/pic7.jpg" />7</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/plant.jpg" />8</a>
+<a href="#" title=""><img src="includes/overall/data1/tooltips/technology.jpg"  />9</a>
+</div></div>
+	</div>
+	<script type="text/javascript" src="includes/overall/engine1/wowslider.js"></script>
+	<script type="text/javascript" src="includes/overall/engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
 
  </body>
 </html>
